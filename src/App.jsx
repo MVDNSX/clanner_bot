b8d7f638e0f129dbf260106095d94e6b7bda10a0
MVDNS <a.s.meleshenkov@gmail.com>
2025-03-27 21:34:58 +0300

    14

--- src/App.jsx
+++ src/App.jsx
@@ -5,34 +5,73 @@ import './App.css'
 
 function App() {
 
+  const [role, setRole] = useEffect(null)
+
   const tg = window.Telegram.WebApp
   const data = {
     id: tg.initDataUnsafe?.user?.id,
     username:tg.initDataUnsafe?.user?.username
   }
+
+
+  const fetchRole = async (data) => {
+    try {
+      const response = await fetch('https://clanner-server.onrender.com/web-data', {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(data)
+      })
+      if(!response.ok){
+        throw new Error('Ошибка запроса роли')
+      }
+       const res = await response.json();
+       setRole(res.role)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   useEffect(()=>{
   tg.ready()
-  
-  fetch('https://clanner-server.onrender.com/web-data', {
-    method: 'POST',
-    headers: {'Content-Type': 'application/json' 
-    },
-    body: JSON.stringify(data)
-  })
+  fetchRole(data)
 },[])
 
 const onClose = () => {
   tg.close()
 }
 
+if (role === 'notInClan') {
   return (
     <>
-      work!!!
-      <button onClick={onClose}>Закрыть!!!</button>
-      <div>{tg.initDataUnsafe?.user?.id}</div>
-      <div>{tg.initDataUnsafe?.user?.username}</div>
+      <div>Форма для вступающих</div>
+      <button onClick={onClose}>Закрыть</button>
     </>
   )
+}
+if (role === 'inClan') {
+  return (
+    <>
+      <div>Шаблон для членов клана</div>
+      <button onClick={onClose}>Закрыть</button>
+    </>
+  )
+}
+if (role === 'officerClan') {
+  return (
+    <>
+      <div>Шаблон для офицеров</div>
+      <button onClick={onClose}>Закрыть</button>
+    </>
+  )
+}
+
+return (
+  <>
+    Ошибка сервиса
+    <button onClick={onClose}>Закрыть</button>
+  </>
+)
+
 }
 
 export default App
