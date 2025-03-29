5cf7a5fefb1c677e708a44bd62eaac1028e80d95
MVDNS <a.s.meleshenkov@gmail.com>
2025-03-29 15:27:10 +0300

    21

--- src/App.jsx
+++ src/App.jsx
@@ -6,27 +6,16 @@ import FormEntry from './components/FormEntry/FormEntry'
 
 
 function App() {
-  useEffect(() => {
-    const initTelegramApp = () => {
-      if (window.Telegram && window.Telegram.WebApp) {
-        window.Telegram.WebApp.init();
-        window.Telegram.WebApp.expand();
-      }
-    };
-
-    setTimeout(initTelegramApp, 500);
-
-    return () => {
-      if (window.Telegram && window.Telegram.WebApp) {
-        window.Telegram.WebApp.collapse();
-      }
-    };
-  }, []);
+  const tg = window.Telegram?.WebApp
+  useEffect(()=>{
+    tg.expand()
+  }, [])
 
 return (
-  <div className='layout'>
-    <FormEntry />
-  </div>
+  <>
+  123
+    {/*<FormEntry />*/}
+  </>
 )
 }
 
