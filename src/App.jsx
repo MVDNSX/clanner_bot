4594fc33746b2a8cbf87504b6ad13e30d0d6f99d
MVDNS <a.s.meleshenkov@gmail.com>
2025-03-29 17:54:33 +0300

    21

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,23 @@ import FormEntry from './components/FormEntry/FormEntry'
 
 function App() {
   
-  useEffect(()=>{
+  useEffect(() => {
     const tg = window.Telegram?.WebApp;
+    if (!tg) return;
 
-    tg.expand()
+    tg.expand();
 
-  }, [])
+    const updateHeight = () => {
+      const keyboardHeight = tg.viewportStableHeight - tg.viewportHeight;
+      document.documentElement.style.setProperty("--tg-height", `${tg.viewportHeight}px`);
+      document.documentElement.style.setProperty("--keyboard-height", `${keyboardHeight}px`);
+    };
+
+    tg.onEvent("viewportChanged", updateHeight);
+    updateHeight();
+
+    return () => tg.offEvent("viewportChanged", updateHeight);
+  }, []);
 
 return (
   <>
