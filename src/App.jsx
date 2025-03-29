cb6fb3ec0b69a0753e9dc8136e6f98b4df4ca5a6
MVDNS <a.s.meleshenkov@gmail.com>
2025-03-29 15:01:08 +0300

    21

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 
+import { useEffect } from 'react'
 import './App.css'
 import FormEntry from './components/FormEntry/FormEntry'
 
 
 
 function App() {
+  useEffect(() => {
+    if (window.Telegram && window.Telegram.WebApp) {
+      window.Telegram.WebApp.init();
+      window.Telegram.WebApp.expand();
+    }
+  },[])
+
 return (
   <div className='layout'>
     <FormEntry />
